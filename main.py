0aaa1028df81d05b79da1c62f082532b49255db4
Manthanjaiswal007 <divinemasti007@gmail.com>
2025-03-29 15:06:16 +0530

    layout and buttons
    The basic layout and buttons are done for the first page......more to be added.

--- main.py
+++ main.py
@@ -1,3 +1,20 @@
 import pygame
+from tkinter as *
+import tkinter as tk
+from tkinter import ttk
 
+root=tk.Tk
+root.geometry('5000x5000')
+root.title("Snake Game")
+Lframe=Frame(root)
+Gframe=Frame(root)
+Lframe.pack()
+
+single=Tk.Button(Lframe, Text="Single Player", bg='Orange', font='Chewy 40', height=3)
+single.pack()
+
+multi=Tk.Button(Lframe, Text="Multi Player", bg='Blue', font='Chewy 40', height=3)
+multi.pack()
+
+root.mainloop()
 pygame.init()
