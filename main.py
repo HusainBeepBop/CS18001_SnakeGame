23587ab3275d7f6da09e0441e9776de511e6be69
Husain <husainlokhandwala33@gmail.com>
2025-04-05 12:32:36 +0530

    add description section and fixed the color

--- main.py
+++ main.py
@@ -5,8 +5,12 @@ from PIL import Image, ImageTk
 
 root = tk.Tk()
 root.title("Snake Game")
-root.geometry("800x600")
-root.configure(bg="#1f1f1f")
+root.attributes('-fullscreen', True)
+root.configure(bg="#181818")
+
+des_section = tk.Frame(root, borderwidth=2, relief="groove")
+des_section.pack(side=tk.LEFT, fill=tk.Y, padx=10, pady=10)
+des_section.configure(bg="#1f1f1f")
 
 root.mainloop()
 pygame.init()
