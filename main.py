fb35b1240e148698bb6703144c556f971cb4c6fb
Husain <husainlokhandwala33@gmail.com>
2025-04-05 13:47:33 +0530

    made the button and applied style

--- main.py
+++ main.py
@@ -19,17 +19,27 @@ des_section.pack(side=tk.LEFT, fill=tk.Y, padx=10, pady=10)
 des_section.configure(bg="#f0f0f0")
 des_section.pack_propagate(False)
 
+photoWidth = int(des_width * 0.9)
+photoHeight = int(des_height * 0.4)
 
-logo_frame = tk.Frame(des_section)
+logo_frame = tk.Frame(des_section, relief="raised")
 logo_frame.pack(pady=(10, 5))
-logo_width = int(des_width * 0.9)
-logo_height = int(des_height * 0.4)
 logo_image = Image.open("./assets/logo.png")
-logo_image = logo_image.resize((logo_width, logo_height), Image.Resampling.LANCZOS)
+logo_image = logo_image.resize((photoWidth, photoHeight), Image.Resampling.LANCZOS)
 
 logo = ImageTk.PhotoImage(logo_image)
 logo_label = tk.Label(logo_frame, image=logo)
 logo_label.pack()
+
+team_frame = tk.Frame(des_section, relief="raised")
+team_frame.pack(pady=(5, 10))
+team_frame.configure(bg="#181818")
+team_frame.pack_propagate(False)
+team_frame.configure(width=photoWidth, height=photoHeight)
+
+infoButton = tk.Button(des_section, text="Info", bg="#181818", fg="#f0f0f0", relief="raised")
+infoButton.pack(pady=(5, 5), padx=(5, 5), side=tk.BOTTOM)
+infoButton.configure(width=photoWidth)
 
 root.mainloop()
 pygame.init()
