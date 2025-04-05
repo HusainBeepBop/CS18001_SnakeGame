f81ba3a1b4dea2d6978b2e175df807d8358d9323
Husain <husainlokhandwala33@gmail.com>
2025-04-05 14:01:07 +0530

    less padding

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ desWidth = int(vpWidth * 0.25)
 desHeight = int(vpheight * 0.9)
 
 des_section = tk.Frame(root, width=desWidth, height=desHeight, borderwidth=5, relief="raised")
-des_section.pack(side=tk.LEFT, fill=tk.Y, padx=10, pady=10)
+des_section.pack(side=tk.LEFT, fill=tk.Y, padx=5, pady=5)
 des_section.configure(bg="#f0f0f0")
 des_section.pack_propagate(False)
 
@@ -23,7 +23,7 @@ photoWidth = int(desWidth * 0.9)
 photoHeight = int(desHeight * 0.4)
 
 logo_frame = tk.Frame(des_section, relief="raised")
-logo_frame.pack(pady=(10, 5))
+logo_frame.pack(pady=(5, 5))
 logo_image = Image.open("./assets/logo.png")
 logo_image = logo_image.resize((photoWidth, photoHeight), Image.Resampling.LANCZOS)
 
@@ -32,8 +32,8 @@ logo_label = tk.Label(logo_frame, image=logo)
 logo_label.pack()
 
 team_frame = tk.Frame(des_section, relief="raised")
-team_frame.pack(pady=(5, 10))
-team_frame.configure(bg="#181818")
+team_frame.pack(pady=(0, 5))
+team_frame.configure(bg="#f0f0f0")
 team_frame.pack_propagate(False)
 team_frame.configure(width=photoWidth, height=photoHeight)
 
@@ -41,8 +41,8 @@ infoButton = tk.Button(des_section, text="Info", bg="#181818", fg="#f0f0f0", rel
 infoButton.pack(pady=(5, 5), padx=(5, 5), side=tk.BOTTOM)
 infoButton.configure(width=photoWidth)
 
-main_area = tk.Frame(root, borderwidth=5, relief="groove")
-main_area.pack(side=tk.RIGHT, fill=tk.BOTH, expand=True, padx=10, pady=10)
+main_area = tk.Frame(root, borderwidth=5, relief="raised")
+main_area.pack(side=tk.RIGHT, fill=tk.BOTH, expand=True, padx=(0,5), pady=5)
 main_area.configure(bg="#1f1f1f")
 
 
