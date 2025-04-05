496e524fb103c148a36af213c3656a3be9260f12
Husain <husainlokhandwala33@gmail.com>
2025-04-05 14:50:09 +0530

    made a frame and added start game button

--- main.py
+++ main.py
@@ -41,10 +41,31 @@ infoButton = tk.Button(des_section, text="Info", bg="#181818", fg="#f0f0f0", rel
 infoButton.pack(pady=(5, 5), padx=(5, 5), side=tk.BOTTOM)
 infoButton.configure(width=photoWidth)
 
-main_area = tk.Frame(root, borderwidth=5, relief="raised")
-main_area.pack(side=tk.RIGHT, fill=tk.BOTH, expand=True, padx=(0,5), pady=5)
-main_area.configure(bg="#1f1f1f")
+content_section = tk.Frame(root, borderwidth=5, relief="raised")
+content_section.pack(side=tk.RIGHT, fill=tk.BOTH, expand=True, padx=(0,5), pady=5)
+content_section.configure(bg="#1f1f1f")
 
+close_game = tk.Frame(content_section)
+close_game.pack(side=tk.TOP, anchor=tk.NE)
+close_game.configure(width=content_section.winfo_width(), height=content_section.winfo_height() * 0.06)
+
+
+close_button = tk.Button(close_game, text="X", font=('Arial',16), bg="#181818", fg="#f0f0f0", relief="raised", command=root.destroy)
+close_button.pack(side=tk.LEFT, anchor=tk.NE)
+close_button.pack_propagate(False)
+
+main_area = tk.Frame(content_section)
+main_area.pack(side=tk.BOTTOM, fill=tk.BOTH, expand=True)
+
+start_game = tk.Frame(main_area)
+start_game.configure(width=500, height=1000)
+start_game.pack(anchor=tk.CENTER, expand=True)
+
+single_player = tk.Button(start_game, text="Single Player", bg="#181818", fg="#f0f0f0", relief="raised")
+single_player.pack(fill=tk.BOTH, expand=True)
+
+dual_player = tk.Button(start_game, text="Dual Player", bg="#181818", fg="#f0f0f0", relief="raised")
+dual_player.pack(fill=tk.BOTH, expand=True)
 
 root.mainloop()
 pygame.init()
